2f22a6b6bc4dd0717f237fc59159e7864fbabc54
attia0079 <abdallah.mohamed0079@gmail.com>
2024-03-17 00:41:08 +0200

    added indexes code.

---
+++ northwind-dwh/indexes.sql
@@ -0,0 +1,25 @@
+CREATE INDEX idx_city_country_btree ON redesign_sales_datamart.customers_dim USING btree (cust_city, cust_country);
+
+CREATE INDEX idx_emp_name ON redesign_sales_datamart.employees_dim USING hash (emp_name);
+
+CREATE INDEX idx_shipper_company_name_hash ON redesign_sales_datamart.shippers_dim USING hash (company_name);
+
+
+CREATE INDEX idx_company_name ON redesign_sales_datamart.shippers_dim (company_name);
+
+
+CREATE INDEX idx_product_name_hash ON redesign_sales_datamart.products_dim USING hash (product_name);
+CREATE INDEX idx_category_name_hash ON redesign_sales_datamart.products_dim USING hash (category_name);
+CREATE INDEX idx_supplier_name_hash ON redesign_sales_datamart.products_dim USING hash (supplier_name);
+
+
+
+CREATE INDEX idx_product_name ON redesign_sales_datamart.sales_fact (product_sk);
+CREATE INDEX idx_order_date ON redesign_sales_datamart.sales_fact (order_date_sk);
+CREATE INDEX idx_supplier ON redesign_sales_datamart.sales_fact (shipper_sk);
+CREATE INDEX idx_customer ON redesign_sales_datamart.sales_fact (cust_sk);
+CREATE INDEX idx_employee ON redesign_sales_datamart.sales_fact (emp_sk);
+CREATE INDEX idx_quantity ON redesign_sales_datamart.sales_fact (quantity_per_product);
+CREATE INDEX idx_discount ON redesign_sales_datamart.sales_fact (discount);
+CREATE INDEX idx_total_price ON redesign_sales_datamart.sales_fact (total_price);
+CREATE INDEX idx_freight ON redesign_sales_datamart.sales_fact (freight);
